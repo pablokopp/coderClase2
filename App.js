fb487b54ddd4637a57c7827022556b5247437824
pablokopp <84154261+pablokopp@users.noreply.github.com>
2022-01-05 15:21:40 -0300

    Styles

--- App.js
+++ App.js
@@ -7,93 +7,135 @@
  */
 
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+  Button,
+  FlatList,
   StyleSheet,
   Text,
+  TextInput,
+  TouchableOpacity,
   View,
-  useColorScheme,
 } from 'react-native';
+import React, {useState} from 'react';
 
-import type {Node} from 'react';
-import React from 'react';
+const App = () => {
+  const [task, setTask] = useState('');
+  const [itemList, setItemList] = useState([]);
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const onChange = text => {
+    setTask(text);
+  };
+  const addTask = () => {
+    setItemList([...itemList, {id: Math.random() + 1, task}]);
+    setTask('');
+  };
+
+  /*el filter devuelve un array con todos los item que tengan un id distinto al que le paso a la funcion, filtrando asi el item que quiero eliminar en cuestion */
+  const deleteTask = id => {
+    setItemList(itemList.filter(task => task.id !== id));
+  };
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <View style={styles.formContainer}>
+        <TextInput
+          autoFocus
+          style={styles.textInput}
+          placeholder="do something"
+          onChangeText={text => onChange(text)}
+          value={task}></TextInput>
+        <Button
+          title="SEND"
+          color="#C39DE8"
+          onPress={addTask}
+          disabled={task.trim() === ''}
+        />
+      </View>
+      {itemList.length > 0 ? (
+        <View style={styles.listContainer}>
+          {/* flatlist es un componente que de manera optimizada me devuelve una lista, hace el map y eso */}
+          <FlatList
+            /*que es un keyExtractor??? --> le da un key unico a cada item de la lista? entonces no es necesario agregar el key al view mas abajo
+            
+            cito al profe: Este prop lo que hace es estrae por nosotros cual va a ser el identificador unico (key) de esta lista.
+            */
+            keyExtractor={item => item.id}
+            data={itemList}
+            renderItem={({item, index}) => {
+              return (
+                <View style={styles.listItem}>
+                  <Text style={styles.listItemText}>{item.task}</Text>
+                  <TouchableOpacity onPress={() => deleteTask(item.id)}>
+                    <Text style={styles.listItemDelete}>✖</Text>
+                  </TouchableOpacity>
+                </View>
+              );
+            }}
+          />
+
+          <Button
+            style={styles.listBtnClear}
+            onPress={() => setItemList([])}
+            title="CLEAR LIST"
+            color="#C39DE8"
+          />
+        </View>
+      ) : (
+        // si no hay lista muestro esto
+        <Text style={styles.listTextEmpty}>Empty List 😔</Text>
+      )}
     </View>
   );
 };
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Mi primer App">
-            <Text>Hola CoderMundo!</Text>
-          </Section>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
+// falta dar estilos a la lista y a text de empty list
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: '#f7f7f2',
   },
-  sectionTitle: {
-    fontSize: 26,
-    fontWeight: '900',
+  formContainer: {
+    flexDirection: 'row',
+    padding: 20,
+    justifyContent: 'space-around',
+    alignItems: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
+  textInput: {
+    flex: 0.8,
     fontSize: 18,
-    fontWeight: '400',
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
   },
-  highlight: {
-    fontWeight: '700',
+
+  listContainer: {
+    flexDirection: 'column',
+    padding: 20,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  listTextEmpty: {
+    textAlign: 'center',
+    fontSize: 20,
+    marginTop: 40,
+  },
+  listItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderWidth: 3,
+    borderRadius: 5,
+    borderColor: '#C39DE8',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    marginVertical: 15,
+  },
+  listItemText: {
+    fontSize: 20,
+    flexBasis: '85%',
+    textTransform: 'capitalize',
+  },
+  listItemDelete: {
+    display: 'flex',
+    color: '#C39DE8',
+    fontSize: 20,
+    margin: 10,
   },
 });
 
